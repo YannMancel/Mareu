1d1a765681876274d53e9fc7db86a680cedf6451
Yann Mancel <yann.mancel@hotmail.fr>
2019-08-15 10:26:43 +0200

    Update hour filter
    Update file:
    - HoursFilterFragment (Java class, update hour filter)
    
    Delete file:
    - DateFilterDialog (Java class)

--- app/src/main/java/com/mancel/yann/mareu/ui/dialogFragments/HoursFilterFragment.java
+++ app/src/main/java/com/mancel/yann/mareu/ui/dialogFragments/HoursFilterFragment.java
@@ -6,10 +6,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.NumberPicker;
 
 import com.mancel.yann.mareu.R;
 import com.mancel.yann.mareu.base.BaseDialogFragment;
+import com.mancel.yann.mareu.utils.TimeTools;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -23,7 +26,14 @@ public class HoursFilterFragment extends BaseDialogFragment {
 
     // FIELDS --------------------------------------------------------------------------------------
 
-
+    @BindView(R.id.dialog_filter_date_np_min_hour)
+    NumberPicker mMinHour;
+    @BindView(R.id.dialog_filter_date_np_min_minute)
+    NumberPicker mMinMinute;
+    @BindView(R.id.dialog_filter_date_np_max_hour)
+    NumberPicker mMaxHour;
+    @BindView(R.id.dialog_filter_date_np_max_minute)
+    NumberPicker mMaxMinute;
 
     // CONSTRUCTORS --------------------------------------------------------------------------------
 
@@ -44,6 +54,9 @@ public class HoursFilterFragment extends BaseDialogFragment {
         // Using the ButterKnife library
         ButterKnife.bind(this, view);
 
+        // Configures the design
+        this.configureDesign();
+
         // Creates Alert Dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
@@ -51,7 +64,10 @@ public class HoursFilterFragment extends BaseDialogFragment {
         builder.setView(view)
                .setTitle(getString(R.string.filter_hour))
                .setPositiveButton(getString(R.string.yes),
-                        (dialog, which) -> {this.mCallback.selectHoursForFilter("08:00","9:30");})
+                        (dialog, which) -> {
+                            this.mCallback.selectHoursForFilter(TimeTools.convertHourAndMinuteToString(this.mMinHour.getValue(), this.mMinMinute.getValue(), ":"),
+                                                                TimeTools.convertHourAndMinuteToString(this.mMaxHour.getValue(), this.mMaxMinute.getValue(), ":"));
+                        })
                .setNegativeButton(getString(R.string.no),
                         (dialog, which) -> {});
 
@@ -62,9 +78,36 @@ public class HoursFilterFragment extends BaseDialogFragment {
 
     /**
      * Returns a {@link HoursFilterFragment}
+     *
      * @return a {@link HoursFilterFragment}
      */
     public static HoursFilterFragment newInstance() {
         return new HoursFilterFragment();
+    }
+
+    // NUMBER PICKERS ******************************************************************************
+
+    /**
+     * Configures the {@link NumberPicker}
+     */
+    private void configureNumberPicker(NumberPicker numberPicker, int minValue,  int maxValue, int currentValue) {
+        numberPicker.setMinValue(minValue);
+        numberPicker.setMaxValue(maxValue);
+        numberPicker.setValue(currentValue);
+    }
+
+    // UI ******************************************************************************************
+
+    /**
+     * Configures the design
+     */
+    private void configureDesign() {
+        // HOUR
+        configureNumberPicker(this.mMinHour, 0, 23, 8);
+        configureNumberPicker(this.mMaxHour, 0, 23, 20);
+
+        // MINUTE
+        configureNumberPicker(this.mMinMinute, 0, 59, 0);
+        configureNumberPicker(this.mMaxMinute, 0, 59, 30);
     }
 }
